0e9263b6b5f39c115eabff12c6b08a7651aad12a
Evans Shango <evansonshango@gmail.com>
2023-03-19 12:47:14 +0300

    fix: Add effect to image transformation (#17)
    * fix: Add effect to image transformation

--- Services/CloudinaryService.cs
+++ Services/CloudinaryService.cs
@@ -14,15 +14,15 @@ public class CloudinaryService : ICloudinaryService {
 
     public async Task<ImageUploadResult?> UploadFile(IFormFile file, string itemId, int? width, int? height) {
         if (file.Length <= 0) return null;
-
-        var extension = file.FileName.Split(".")[file.FileName.Split(".").Length - 1];
-        var fileName = $"{DateTime.Now:yyyyMMddHHmmssffff}.{extension.ToLower()}";
+        
+        var fileName = $"{DateTime.Now:yyyyMMddHHmmssffff}.png";
 
         await using var stream = file.OpenReadStream();
         var uploadParams = new ImageUploadParams {
             File = new FileDescription(fileName, stream),
             Folder = itemId,
-            Transformation = new Transformation().Width(width).Height(height).Crop("fill").Gravity("face"),
+            Transformation = new Transformation().Effect("background_removal")
+                .Width(width).Height(height).Crop("fill").Gravity("face")
         };
 
         return await _cloudinary.UploadAsync(uploadParams);
